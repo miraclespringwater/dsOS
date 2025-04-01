c19a550c1d160253e672f57080b3a76f8bbcbf0f
George Colletti Barajas <miracle@dreamsmith.net>
2025-04-01 18:01:53 -0500

    fix pacman command

--- pkgs.sh
+++ pkgs.sh
@@ -2,7 +2,7 @@
 
 if [[ -e "pkglist.txt" ]]; then
   echo "Installing packages from official repos...";
-  sudo pacman -Syu --needed --noconfirm git base-devel - < pkglist.txt 
+  sudo pacman -Syu --needed git base-devel - < pkglist.txt 
 else
   echo "No package list available! (pkglist.txt missing)"
 fi
