69b9c0f26a0326dc78b952443a4f998cf8890f1b
George Colletti Barajas <miracle@dreamsmith.net>
2025-04-01 20:09:14 -0500

    try to speed up aur pkgs install

--- aur-pkgs.sh
+++ aur-pkgs.sh
@@ -2,7 +2,7 @@
 
 if [[ -e "aur-pkglist.txt" ]]; then
   echo "Installing packages from AUR (community repos)...";
-  yay -S --needed - < aur-pkglist.txt 
+  yay -S --needed --mflags "--ignore python2" - < aur-pkglist.txt 
 else
   echo "No package list available! (aur-pkglist.txt missing)"
 fi
